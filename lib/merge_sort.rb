c118ca0963cff35979af0cccf59a45be70067d6d
Your Name <you@example.com>
2025-04-29 09:40:17 +0100

    refactor code for better functionality

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -1,25 +1,24 @@
+# frozen_string_literal: true
+
 def merge(left, right)
   new_arr = []
-  until left.empty? || right.empty? do
-    if left[0] < right[0]
-      new_arr << left.shift
-    else
-      new_arr << right.shift
-    end
+  until left.empty? || right.empty?
+    new_arr << if left[0] < right[0]
+                 left.shift
+               else
+                 right.shift
+               end
   end
   puts new_arr
-  return new_arr + left + right
+  new_arr + left + right
 end
 
 def merge_sort(array)
-    return array if array.length <= 1
-  
-    a = merge_sort(array.slice(0, array.length/2)) 
-    b = merge_sort(array.slice(array.length/2, array.length))
-  
-   merge(a, b)
+  return array if array.length <= 1
+
+  a = merge_sort(array.slice(0, array.length / 2))
+  b = merge_sort(array.slice(array.length / 2, array.length))
+  merge(a, b)
 end
-
-
 
 merge_sort([3, 2, 1, 13, 8, 5, 0, 1])
